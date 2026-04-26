5a6df4702484e47fd99d30464c82e39d6332f975
JotaWorks <jay@jota.works>
2026-04-26 07:42:06 -0600

    Add per-page CSS architecture, loader template, updated SCSS structure

---
+++ template-parts/loader.php
@@ -0,0 +1,9 @@
+<?php
+/*
+* Reusable Block for Loader Screen
+*/
+?>
+<div id="loader">
+    <img src="<?php echo esc_url( get_theme_mod( 'loader_logo' ) ); ?>" alt="<?php bloginfo( 'name' ); ?>" />
+    <p>LOADING...</p>
+</div>
